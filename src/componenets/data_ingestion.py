e6017a63882af1d585f8e0aa4fa03a99c6bae816
gargie25 <gargi.dhulekar23@spit.ac.in>
2025-08-13 23:01:18 +0530

    Model trainedcls

--- src/componenets/data_ingestion.py
+++ src/componenets/data_ingestion.py
@@ -10,6 +10,9 @@ from dataclasses import dataclass
 from src.componenets.data_transformation import DataTranformation
 from src.componenets.data_transformation import DataTransformationconfig
 
+from src.componenets.model_trainer import ModelTrainerConfig
+from src.componenets.model_trainer import ModelTrainer
+
 
 @dataclass
 class DataIngestionconfig:
@@ -49,5 +52,9 @@ class DataIngestion:
 if __name__=="__main__":
     obj= DataIngestion()
     train_data,test_data,_=obj.initiate_data_ingestion()
+
     data_transformation= DataTranformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer= ModelTrainer()
+    print(model_trainer.initiate_model_trainer(train_arr,test_arr))
